f2b4e49e2adf8b1495eb26b329e4e6f8700ae756
eltonpeixoto <56564311+eltonpeixoto@users.noreply.github.com>
2022-02-10 10:46:16 -0300

    Add files via upload

---
+++ export_fdb.py
@@ -0,0 +1,13 @@
+import fdb
+import pandas as pd
+
+con = fdb.connect(host="localhost", database=r"C:\Program Files\Firebird\Firebird_3_0\SFPB0001.FDB",user='sysdba', password='teste123')
+
+
+sql_query = pd.read_sql_query('''
+                              select * from SFPTB071_EMPRESARE
+                              '''
+                              ,con) # here, the 'con' is the variable that contains your database connection information from step 2
+
+df = pd.DataFrame(sql_query)
+df.to_csv (r'C:\Users\Dejair\Desktop\export_data.csv', index = False) # place 'r' before the path name to avoid any errors in the path(editado)
