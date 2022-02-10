79eac0c38447e55edd3d0ee249e0baf5181b458c
eltonpeixoto <56564311+eltonpeixoto@users.noreply.github.com>
2022-02-10 10:46:53 -0300

    Update export_fdb.py

--- export_fdb.py
+++ export_fdb.py
@@ -10,4 +10,4 @@ sql_query = pd.read_sql_query('''
                               ,con) # here, the 'con' is the variable that contains your database connection information from step 2
 
 df = pd.DataFrame(sql_query)
-df.to_csv (r'C:\Users\Dejair\Desktop\export_data.csv', index = False) # place 'r' before the path name to avoid any errors in the path(editado)
+df.to_csv (r'C:\Users\elton\Desktop\export_data.csv', index = False) # place 'r' before the path name to avoid any errors in the path(editado)
